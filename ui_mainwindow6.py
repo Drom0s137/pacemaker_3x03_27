56868fe41751ccdb5c8d72765ab88d810ca89af7
pannuj1 <90873745+pannuj1@users.noreply.github.com>
2022-10-23 23:42:19 -0400

    Complete GUI

---
+++ ui_mainwindow6.py
@@ -0,0 +1,290 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'mainwindow.ui'
+##
+## Created by: Qt User Interface Compiler version 6.3.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QFrame, QGroupBox, QHBoxLayout,
+    QLabel, QMainWindow, QPushButton, QSizePolicy,
+    QSpinBox, QStackedWidget, QVBoxLayout, QWidget)
+
+from pyqtgraph import PlotWidget
+import numpy as np
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(1000, 500)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.verticalLayout = QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setSpacing(0)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.Top_Bar = QFrame(self.centralwidget)
+        self.Top_Bar.setObjectName(u"Top_Bar")
+        self.Top_Bar.setMaximumSize(QSize(16777215, 40))
+        self.Top_Bar.setAutoFillBackground(False)
+        self.Top_Bar.setStyleSheet(u"background-color: rgb(45, 45, 45);")
+        self.Top_Bar.setFrameShape(QFrame.NoFrame)
+        self.Top_Bar.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout = QHBoxLayout(self.Top_Bar)
+        self.horizontalLayout.setSpacing(0)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.frame_toggle = QFrame(self.Top_Bar)
+        self.frame_toggle.setObjectName(u"frame_toggle")
+        self.frame_toggle.setMaximumSize(QSize(70, 40))
+        self.frame_toggle.setStyleSheet(u"background-color: rgb(85, 170, 255);")
+        self.frame_toggle.setFrameShape(QFrame.StyledPanel)
+        self.frame_toggle.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_2 = QVBoxLayout(self.frame_toggle)
+        self.verticalLayout_2.setSpacing(0)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.Btn_Toggle = QPushButton(self.frame_toggle)
+        self.Btn_Toggle.setObjectName(u"Btn_Toggle")
+        sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.Btn_Toggle.sizePolicy().hasHeightForWidth())
+        self.Btn_Toggle.setSizePolicy(sizePolicy)
+        self.Btn_Toggle.setStyleSheet(u"color: rgb(255, 255, 255);\n"
+"border: 0px solid;")
+
+        self.verticalLayout_2.addWidget(self.Btn_Toggle)
+
+
+        self.horizontalLayout.addWidget(self.frame_toggle)
+
+        self.frame_top = QFrame(self.Top_Bar)
+        self.frame_top.setObjectName(u"frame_top")
+        self.frame_top.setFrameShape(QFrame.StyledPanel)
+        self.frame_top.setFrameShadow(QFrame.Raised)
+
+        self.horizontalLayout.addWidget(self.frame_top)
+
+
+        self.verticalLayout.addWidget(self.Top_Bar)
+
+        self.Content = QFrame(self.centralwidget)
+        self.Content.setObjectName(u"Content")
+        self.Content.setStyleSheet(u"background-color: rgb(72, 72, 72);")
+        self.Content.setFrameShape(QFrame.NoFrame)
+        self.Content.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_2 = QHBoxLayout(self.Content)
+        self.horizontalLayout_2.setSpacing(0)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.frame_left_menu = QFrame(self.Content)
+        self.frame_left_menu.setObjectName(u"frame_left_menu")
+        self.frame_left_menu.setMinimumSize(QSize(70, 0))
+        self.frame_left_menu.setMaximumSize(QSize(70, 16777215))
+        self.frame_left_menu.setStyleSheet(u"background-color: rgb(45, 45, 45);")
+        self.frame_left_menu.setFrameShape(QFrame.StyledPanel)
+        self.frame_left_menu.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_3 = QVBoxLayout(self.frame_left_menu)
+        self.verticalLayout_3.setSpacing(6)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.verticalLayout_3.setContentsMargins(0, 0, 0, 0)
+        self.frame_top_menus = QFrame(self.frame_left_menu)
+        self.frame_top_menus.setObjectName(u"frame_top_menus")
+        self.frame_top_menus.setFrameShape(QFrame.NoFrame)
+        self.frame_top_menus.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_4 = QVBoxLayout(self.frame_top_menus)
+        self.verticalLayout_4.setSpacing(0)
+        self.verticalLayout_4.setObjectName(u"verticalLayout_4")
+        self.verticalLayout_4.setContentsMargins(0, 0, 0, 0)
+        self.btn_page_4 = QPushButton(self.frame_top_menus)
+        self.btn_page_4.setObjectName(u"btn_page_4")
+        self.btn_page_4.setMinimumSize(QSize(0, 40))
+        self.btn_page_4.setStyleSheet(u"QPushButton{\n"
+"	color: rgb(255, 255, 255);\n"
+"	background-color: rgb(45, 45, 45);\n"
+"	border: 0px solid;\n"
+"}\n"
+"")
+
+        self.verticalLayout_4.addWidget(self.btn_page_4)
+
+        self.btn_page_3 = QPushButton(self.frame_top_menus)
+        self.btn_page_3.setObjectName(u"btn_page_3")
+        self.btn_page_3.setMinimumSize(QSize(0, 40))
+        self.btn_page_3.setStyleSheet(u"QPushButton{\n"
+"	color: rgb(255, 255, 255);\n"
+"	background-color: rgb(45, 45, 45);\n"
+"	border: 0px solid;\n"
+"}\n"
+"")
+
+        self.verticalLayout_4.addWidget(self.btn_page_3)
+
+        self.btn_page_2 = QPushButton(self.frame_top_menus)
+        self.btn_page_2.setObjectName(u"btn_page_2")
+        self.btn_page_2.setMinimumSize(QSize(0, 40))
+        self.btn_page_2.setStyleSheet(u"QPushButton{\n"
+"	color: rgb(255, 255, 255);\n"
+"	background-color: rgb(45, 45, 45);\n"
+"	border: 0px solid;\n"
+"}\n"
+"")
+
+        self.verticalLayout_4.addWidget(self.btn_page_2)
+
+        self.btn_page_1 = QPushButton(self.frame_top_menus)
+        self.btn_page_1.setObjectName(u"btn_page_1")
+        self.btn_page_1.setMinimumSize(QSize(0, 40))
+        self.btn_page_1.setStyleSheet(u"QPushButton{\n"
+"	color: rgb(255, 255, 255);\n"
+"	background-color: rgb(45, 45, 45);\n"
+"	border: 0px solid;\n"
+"}\n"
+"")
+
+        self.verticalLayout_4.addWidget(self.btn_page_1)
+
+
+        self.verticalLayout_3.addWidget(self.frame_top_menus, 0, Qt.AlignTop)
+
+
+        self.horizontalLayout_2.addWidget(self.frame_left_menu)
+
+        self.frame_pages = QFrame(self.Content)
+        self.frame_pages.setObjectName(u"frame_pages")
+        self.frame_pages.setFrameShape(QFrame.StyledPanel)
+        self.frame_pages.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_5 = QVBoxLayout(self.frame_pages)
+        self.verticalLayout_5.setObjectName(u"verticalLayout_5")
+        self.Pages_Widget = QStackedWidget(self.frame_pages)
+        self.Pages_Widget.setObjectName(u"Pages_Widget")
+        self.page_1 = QWidget()
+        self.page_1.setObjectName(u"page_1")
+        self.Pages_Widget.addWidget(self.page_1)
+        self.page_4 = QWidget()
+        self.page_4.setObjectName(u"page_4")
+        self.groupBox = QGroupBox(self.page_4)
+        self.groupBox.setObjectName(u"groupBox")
+        self.groupBox.setGeometry(QRect(0, 20, 561, 181))
+        self.groupBox.setStyleSheet(u"color: rgb(255, 255, 255);")
+        self.label_2 = QLabel(self.groupBox)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setGeometry(QRect(20, 30, 101, 21))
+        self.label_3 = QLabel(self.groupBox)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setGeometry(QRect(20, 70, 101, 21))
+        self.label_4 = QLabel(self.groupBox)
+        self.label_4.setObjectName(u"label_4")
+        self.label_4.setGeometry(QRect(20, 110, 101, 20))
+        self.label_5 = QLabel(self.groupBox)
+        self.label_5.setObjectName(u"label_5")
+        self.label_5.setGeometry(QRect(20, 150, 101, 16))
+        self.label_6 = QLabel(self.groupBox)
+        self.label_6.setObjectName(u"label_6")
+        self.label_6.setGeometry(QRect(230, 30, 121, 16))
+        self.label_7 = QLabel(self.groupBox)
+        self.label_7.setObjectName(u"label_7")
+        self.label_7.setGeometry(QRect(230, 70, 131, 16))
+        self.label_8 = QLabel(self.groupBox)
+        self.label_8.setObjectName(u"label_8")
+        self.label_8.setGeometry(QRect(320, 110, 31, 16))
+        self.label_9 = QLabel(self.groupBox)
+        self.label_9.setObjectName(u"label_9")
+        self.label_9.setGeometry(QRect(320, 150, 31, 16))
+        self.spinBox = QSpinBox(self.groupBox)
+        self.spinBox.setObjectName(u"spinBox")
+        self.spinBox.setGeometry(QRect(130, 30, 42, 22))
+        self.spinBox_2 = QSpinBox(self.groupBox)
+        self.spinBox_2.setObjectName(u"spinBox_2")
+        self.spinBox_2.setGeometry(QRect(130, 70, 42, 22))
+        self.spinBox_3 = QSpinBox(self.groupBox)
+        self.spinBox_3.setObjectName(u"spinBox_3")
+        self.spinBox_3.setGeometry(QRect(130, 110, 42, 22))
+        self.spinBox_4 = QSpinBox(self.groupBox)
+        self.spinBox_4.setObjectName(u"spinBox_4")
+        self.spinBox_4.setGeometry(QRect(130, 150, 41, 21))
+        self.spinBox_5 = QSpinBox(self.groupBox)
+        self.spinBox_5.setObjectName(u"spinBox_5")
+        self.spinBox_5.setGeometry(QRect(370, 30, 42, 22))
+        self.spinBox_6 = QSpinBox(self.groupBox)
+        self.spinBox_6.setObjectName(u"spinBox_6")
+        self.spinBox_6.setGeometry(QRect(370, 70, 42, 22))
+        self.spinBox_7 = QSpinBox(self.groupBox)
+        self.spinBox_7.setObjectName(u"spinBox_7")
+        self.spinBox_7.setGeometry(QRect(370, 110, 42, 22))
+        self.spinBox_8 = QSpinBox(self.groupBox)
+        self.spinBox_8.setObjectName(u"spinBox_8")
+        self.spinBox_8.setGeometry(QRect(370, 150, 42, 22))
+        self.label = QLabel(self.page_4)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(390, -10, 111, 31))
+        self.label.setAlignment(Qt.AlignCenter)
+        self.graphicsView = PlotWidget(self.page_4)
+        self.graphicsView.setObjectName(u"graphicsView")
+        self.graphicsView.setGeometry(QRect(0, 210, 631, 221))
+        self.pushButton = QPushButton(self.page_4)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(660, 50, 181, 51))
+        self.pushButton.setStyleSheet(u"color: rgb(255, 255, 255);\n"
+"font: 700 14pt \"Segoe UI\";")
+        self.Pages_Widget.addWidget(self.page_4)
+        self.page_3 = QWidget()
+        self.page_3.setObjectName(u"page_3")
+        self.Pages_Widget.addWidget(self.page_3)
+        self.page_2 = QWidget()
+        self.page_2.setObjectName(u"page_2")
+        self.Pages_Widget.addWidget(self.page_2)
+
+        self.verticalLayout_5.addWidget(self.Pages_Widget)
+
+
+        self.horizontalLayout_2.addWidget(self.frame_pages)
+
+
+        self.verticalLayout.addWidget(self.Content)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+
+        self.Pages_Widget.setCurrentIndex(1)
+
+
+        QMetaObject.connectSlotsByName(MainWindow)
+        
+        
+
+        
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.Btn_Toggle.setText(QCoreApplication.translate("MainWindow", u"Mode", None))
+        self.btn_page_4.setText(QCoreApplication.translate("MainWindow", u"AOO", None))
+        self.btn_page_3.setText(QCoreApplication.translate("MainWindow", u"VOO", None))
+        self.btn_page_2.setText(QCoreApplication.translate("MainWindow", u"AAI", None))
+        self.btn_page_1.setText(QCoreApplication.translate("MainWindow", u"VVI", None))
+        self.groupBox.setTitle(QCoreApplication.translate("MainWindow", u"Parameters", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"Lower Rate Limit", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"Upper Rate Limit", None))
+        self.label_4.setText(QCoreApplication.translate("MainWindow", u"Atrial Amplitude", None))
+        self.label_5.setText(QCoreApplication.translate("MainWindow", u"Atrial Pulse Width", None))
+        self.label_6.setText(QCoreApplication.translate("MainWindow", u"Ventricular Amplitude", None))
+        self.label_7.setText(QCoreApplication.translate("MainWindow", u"Ventricular Pulse Width", None))
+        self.label_8.setText(QCoreApplication.translate("MainWindow", u"VRP", None))
+        self.label_9.setText(QCoreApplication.translate("MainWindow", u"ARP", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"<html><head/><body><p><span style=\" font-size:16pt; font-weight:700; color:#ffffff;\">VVI</span></p></body></html>", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"Create Graph", None))
+    # retranslateUi
+
